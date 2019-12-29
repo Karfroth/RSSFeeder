9e89fa02467faf1ce06fc39df80ce8ee69d6a7cd
Woosang Lee <recollect12@gmail.com>
2019-12-29 00:56:35 -0300

    Update FeedManager members

--- core/src/IFeedManager.fs
+++ core/src/IFeedManager.fs
@@ -4,6 +4,5 @@ module IFeedManager
     type IFeedManager<'key, 'source, 'data>(dispatcher: 'data -> unit) =  // TODO: Replace dispatcher type by correct one
         abstract member Add: 'source -> unit
         abstract member Remove: 'key -> unit
-        abstract member ForceStartSync: unit -> unit
-        abstract member PauseSync: unit -> unit
-        abstract member ResumeSync: unit -> unit
+        abstract member Update: 'key -> unit
+        abstract member UpdateAll: unit -> unit
