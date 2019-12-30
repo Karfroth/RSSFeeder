87a1dff5d03408188be6d2e6243fdfa91c1e3bf4
Woosang Lee <recollect12@gmail.com>
2019-12-29 22:37:12 -0300

    Starting CoreActorDef

--- core/src/RssFeaderCore.fs
+++ core/src/RssFeaderCore.fs
@@ -13,3 +13,30 @@
         | UpdadeFeed of FeedModel.URL
         | UpdateAll
         | RemoveFeed of FeedModel.URL
+        | StartAutoUpdate of int64
+        | StopAutoUpdate
+
+    let handleAddSource (mailbox: Actor<FeedModel.RSSFeedDataSource>) msg =
+        getFeedDataAsync msg |!> mailbox.Sender()
+
+    let coreActorDef (mailbox: Actor<CoreActorMsg>) =
+        let addSourceActorRef = spawn mailbox "add-source-actor" (actorOf2 handleAddSource)
+        let rec loop() = actor {
+            match! mailbox.Receive() with
+            | AddSource source -> 
+                addSourceActorRef <! source
+                return! loop()
+            | AddFeed feedData -> 
+                return! loop()
+            | UpdadeFeed updateFeed -> 
+                return! loop()
+            | UpdateAll -> 
+                return! loop()
+            | RemoveFeed url -> 
+                return! loop()
+            | StartAutoUpdate timeout ->
+                return! loop()
+            | StopAutoUpdate -> 
+                return! loop()
+        }
+        loop()
