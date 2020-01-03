afcee8ded2322d371725beefb93b5fdf3dac92b8
Woosang Lee <recollect12@gmail.com>
2020-01-02 22:03:27 -0300

    Started adding CoreActorTest Module

--- core/src/IDataManager.fs
+++ core/src/IDataManager.fs
@@ -2,7 +2,7 @@ module IDataManager
     type IDataManager<'key> =
         abstract member Add: FeedModel.FeedData -> Async<'key>
         abstract member Remove: 'key -> Async<unit>
-        abstract member Query: 'key -> Async<FeedModel.FeedData>
+        abstract member Query: 'key -> Async<FeedModel.FeedData option>
         abstract member QueryKeys: unit -> Async<'key seq>
         abstract member Update: 'key -> FeedModel.FeedData -> Async<unit>
         abstract member QueryList: 'key seq -> Async<FeedModel.FeedData seq>
