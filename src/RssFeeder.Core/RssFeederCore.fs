7e05eb830d54f05aa2f5e6723ff65d64230c226d
Woosang Lee <recollect12@gmail.com>
2020-03-07 17:29:02 -0300

    clean handleCoreCommand

--- src/RssFeeder.Core/RssFeederCore.fs
+++ src/RssFeeder.Core/RssFeederCore.fs
@@ -48,57 +48,69 @@
             return Removed (Some msg)
         } |!> mailbox.Sender()
 
+    let addSource addSourceActorRef addFeedActorRef source sender mailbox =
+        async {
+            let! feedData = addSourceActorRef <? source
+            let! response = addFeedActorRef <? feedData
+            match response with
+            | Added _ -> sender <! response
+            | _ -> logWarning mailbox "Unexpected response from AddFeedActor"
+        } |> Async.StartImmediate
+
+    let updateFeed updateFeedActorRef updateFeedData sender mailbox =
+        async {
+            let! response = updateFeedActorRef <? updateFeedData
+            match response with 
+            | Updated _ -> sender <! response
+            | _ -> logWarning mailbox "Unexpected respose from UpdateFeedActor"
+        } |> Async.StartImmediate
+
+    let updateAll (dataManager: IDataManager<URL option>) self sender =
+        async {
+            let! keys = dataManager.QueryKeys ()
+            Seq.iter (fun kOption ->
+                match kOption with
+                | Some key ->
+                    async {
+                        let! a = self <? (UpdateFeed key)
+                        sender <! a
+                    } |> Async.StartImmediate
+                | _ -> ignore ()
+            ) keys
+        } |> Async.StartImmediate
+
+    let removeFeed removeFeedActorRef url sender mailbox =
+        async {
+            let! response = removeFeedActorRef <? url
+            match response with
+            | Removed _ -> sender <! response
+            | _ -> logWarning mailbox "Unexpected response from RemoveFeedActor"
+        } |> Async.StartImmediate
+
     let handleCoreCommand (dataManager: IDataManager<URL option>) (mailbox: Actor<CoreActorCommandMsg>) =
         let addSourceActorRef = spawn mailbox "add-source-actor" (actorOf2 handleAddSource)
         let addFeedActorRef = spawn mailbox "add-feed-actor" (actorOf2 (handleAddFeed dataManager))
         let updateFeedActorRef = spawn mailbox "update-feed-actor" (actorOf2 (handleUpdateFeed dataManager))
         let removeFeedActorRef = spawn mailbox "remove-feed-actor" (actorOf2 (handleRemoveFeed dataManager))
 
+
         let rec loop() = actor {
             match! mailbox.Receive() with
             | AddSource source ->
                 let sender = mailbox.Sender()
-                async {
-                    let! feedData = addSourceActorRef <? source
-                    let! response = addFeedActorRef <? feedData
-                    match response with
-                    | Added _ -> sender <! response
-                    | _ -> logWarning mailbox "Unexpected response from AddFeedActor"
-                } |> Async.StartImmediate
+                addSource addSourceActorRef addFeedActorRef source sender mailbox
                 return! loop()
-            | UpdateFeed updateFeed ->
+            | UpdateFeed updateFeedData ->
                 let sender = mailbox.Sender()
-                async {
-                    let! response = updateFeedActorRef <? updateFeed
-                    match response with 
-                    | Updated _ -> sender <! response
-                    | _ -> logWarning mailbox "Unexpected respose from UpdateFeedActor"
-                } |> Async.StartImmediate
+                updateFeed updateFeedActorRef updateFeedData sender mailbox
                 return! loop()
             | UpdateAll ->
-                let self = mailbox.Self
                 let sender = mailbox.Sender()
-                async {
-                    let! keys = dataManager.QueryKeys ()
-                    Seq.iter (fun kOption ->
-                        match kOption with
-                        | Some key ->
-                            async {
-                                let! a = self <? (UpdateFeed key)
-                                sender <! a
-                            } |> Async.StartImmediate
-                        | _ -> ignore ()
-                    ) keys
-                } |> Async.StartImmediate
+                updateAll dataManager mailbox.Self sender
                 return! loop()
             | RemoveFeed url ->
                 let sender = mailbox.Sender()
-                async {
-                    let! response = removeFeedActorRef <? url
-                    match response with
-                    | Removed _ -> sender <! response
-                    | _ -> logWarning mailbox "Unexpected response from RemoveFeedActor"
-                } |> Async.StartImmediate
+                removeFeed removeFeedActorRef url sender mailbox
                 return! loop()
             | StartAutoUpdate timeout ->
                 logWarning mailbox "StartAutoUpdate is not implemented"
