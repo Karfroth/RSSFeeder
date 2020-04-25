f3e6d3b3bb2c7c4df6212b99d3c3d1d6f408fa6f
Woosang Lee <recollect12@gmail.com>
2020-04-24 23:00:15 -0400

    pass db to WPF

--- RssFeeder.Fabulous/RssFeeder.Fabulous.WPF/App.fs
+++ RssFeeder.Fabulous/RssFeeder.Fabulous.WPF/App.fs
@@ -13,9 +13,12 @@ module Main =
     [<STAThread>]
     let main(_args) =
 
+        let systemPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        let dbPath = System.IO.Path.Combine(systemPath, "RssFeeder.db3");
+
         let app = new System.Windows.Application()
         Forms.Init()
         let window = MainWindow() 
-        window.LoadApplication(new RssFeeder.Fabulous.App())
+        window.LoadApplication(new RssFeeder.Fabulous.App(Some dbPath))
 
         app.Run(window)
